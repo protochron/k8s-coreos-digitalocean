7c51fc15cdfc548f775700770b11c52d87022fd3
Douglas Campos <qmx@qmx.me>
2016-10-31 04:18:26 -0200

    remove junk

--- k8s/main.tf
+++ k8s/main.tf
@@ -27,21 +27,6 @@ data "template_file" "kubelet" {
   }
 }
 
-data "template_file" "edge-kubelet" {
-  template = "${file("${path.module}/templates/kubelet.tpl")}"
-
-  vars {
-    etcd_servers     = "${var.etcd_server_urls}"
-    dns_service_ip   = "${var.dns_service_ip}"
-    service_ip_range = "${var.service_ip_range}"
-
-    master      = "http://${digitalocean_droplet.apiserver.ipv4_address_private}:8080"
-    k8s_version = "v${var.kubernetes_version}"
-    apiservers  = "${join(",", formatlist("http://%s:8080", digitalocean_droplet.apiserver.*.ipv4_address_private))}"
-    key         = "${var.do_read_token}"
-  }
-}
-
 resource digitalocean_droplet "apiserver" {
   count              = "${var.apiserver_count}"
   image              = "${var.image}"
